0a50593ea57ec43528100ee7aeca703208f00818
Loading... <158498496+Ash-a9236@users.noreply.github.com>
2024-10-27 20:38:38 -0400

    addStudent()

--- src/main/java/ash_a9236/example/StudentManager.java
+++ src/main/java/ash_a9236/example/StudentManager.java
@@ -1,4 +1,12 @@
 package ash_a9236.example;
 
+import java.util.ArrayList;
+
 public class StudentManager {
+    private ArrayList<Student> students;
+
+    private void addStudent (Student student) {
+
+    }
+
 }
