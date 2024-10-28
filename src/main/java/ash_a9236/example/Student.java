c48e2632e516c60e475a072e45d696a7220d9fc6
Loading... <158498496+Ash-a9236@users.noreply.github.com>
2024-10-27 20:36:36 -0400

    getDescription() method (@Override)

--- src/main/java/ash_a9236/example/Student.java
+++ src/main/java/ash_a9236/example/Student.java
@@ -1,4 +1,12 @@
 package ash_a9236.example;
 
 public class Student extends Person {
+    private int semester;
+    private int id;
+    private double GPA;
+
+    @Override
+    void getDescription() {
+
+    }
 }
