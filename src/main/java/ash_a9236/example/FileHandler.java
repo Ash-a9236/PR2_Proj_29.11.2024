d47c4479c9baef8bb9a81176a45389f7260877cc
Loading... <158498496+Ash-a9236@users.noreply.github.com>
2024-10-27 20:48:41 -0400

    saveToFile() , loadFromFile()

--- src/main/java/ash_a9236/example/FileHandler.java
+++ src/main/java/ash_a9236/example/FileHandler.java
@@ -1,4 +1,11 @@
 package ash_a9236.example;
 
+import java.util.ArrayList;
+
 public interface FileHandler {
+
+    public void saveToFile(String fileName, ArrayList<Student> students);
+
+    public void loadFromFile(String fileName);
+
 }
