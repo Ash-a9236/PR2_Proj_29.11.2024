bf137cf092adf04be85fae04baf0192b9c5ceaf8
Loading... <158498496+Ash-a9236@users.noreply.github.com>
2024-11-07 14:16:51 -0500

    addStudent method

--- src/main/java/ash_a9236/example/StudentManager.java
+++ src/main/java/ash_a9236/example/StudentManager.java
@@ -6,24 +6,41 @@ import java.util.Scanner;
 public class StudentManager {
     private ArrayList<Student> students;
 
-    public void addStudent (Student student) {
-        students.add(student);
+    public void addStudent (Student student) throws StudentNotFoundException {
+        Scanner console = new Scanner(System.in);
+
+        System.out.println("ADDING STUDENT");
+        System.out.print("Enter Student ID : ");
+        int ID = console.nextInt();
+        String IDExtraLine = console.nextLine();
+
+        if (searchStudentByID(ID) != null) {
+            System.out.println("Enter Student Semester : ");
+            int semester = console.nextInt();
+            String SemesterExtraLine = console.nextLine();
+
+            System.out.println("Enter Student GPA : ");
+            double GPA = console.nextDouble();
+            String GPAExtraLine = console.nextLine();
+
+            students.add(new Student(semester, ID, GPA));
+        }
     }
 
-    public void removeStudent (int id) {
-
+    public void removeStudent (int id) throws StudentNotFoundException {
+        students.remove(searchStudentByID(id));
     }
 
 
 
-    public Student searchStudentByID (int id) {
+    public Student searchStudentByID (int id) throws StudentNotFoundException {
         return searchStudentRecursiveHelper(id, 0);
     }
 
-    private Student searchStudentRecursiveHelper (int id, int index) {
+    private Student searchStudentRecursiveHelper (int id, int index) throws StudentNotFoundException {
         //base case
         if (index >= students.size()) {
-            return null;
+            throw new StudentNotFoundException("Student not found!");
         }
 
         //body
