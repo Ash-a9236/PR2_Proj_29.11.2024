4c29539f2e4842a158b21d591234b58608c0f279
Loading... <158498496+Ash-a9236@users.noreply.github.com>
2024-11-07 13:05:02 -0500

    searchStudentByID method + recursion helper searchStudentRecursiveHelper method

--- src/main/java/ash_a9236/example/StudentManager.java
+++ src/main/java/ash_a9236/example/StudentManager.java
@@ -1,20 +1,38 @@
 package ash_a9236.example;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class StudentManager {
     private ArrayList<Student> students;
 
     public void addStudent (Student student) {
-
+        students.add(student);
     }
 
     public void removeStudent (int id) {
 
     }
 
-    public void searchStudentByID (Student student) {
 
+
+    public Student searchStudentByID (int id) {
+        return searchStudentRecursiveHelper(id, 0);
+    }
+
+    private Student searchStudentRecursiveHelper (int id, int index) {
+        //base case
+        if (index >= students.size()) {
+            return null;
+        }
+
+        //body
+        if (students.get(index).getId() == id) {
+            return students.get(index);
+        }
+
+        //recursion
+        return searchStudentRecursiveHelper (id, index + 1);
     }
 
     public ArrayList<Student> sortStudentByID (ArrayList<Student> students) {
